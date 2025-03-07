d6c6184b92e8dad2307f5d65922e1592b64105bf
rexim <reximkut@gmail.com>
2025-03-08 03:59:43 +0700

    Introduce folders src/ and thirdparty/

--- nob.c
+++ nob.c
@@ -1,13 +1,15 @@
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #define NOB_EXPERIMENTAL_DELETE_OLD
-#include "nob.h"
+#include "./thirdparty/nob.h"
 
-#define BUILD_FOLDER "build/"
+#define BUILD_FOLDER      "build/"
+#define SRC_FOLDER        "src/"
+#define THIRDPARTY_FOLDER "thirdparty/"
 
 bool build_tool(Cmd *cmd, const char *bin_path, const char *src_path)
 {
-    cmd_append(cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-o", bin_path, src_path);
+    cmd_append(cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-I"THIRDPARTY_FOLDER, "-o", bin_path, src_path);
     return cmd_run_sync_and_reset(cmd);
 }
 
@@ -16,8 +18,8 @@ int main(int argc, char **argv)
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
     if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
-    if (!build_tool(&cmd, BUILD_FOLDER"bpe", "bpe.c")) return 1;
-    if (!build_tool(&cmd, BUILD_FOLDER"bpe2dot", "bpe2dot.c")) return 1;
+    if (!build_tool(&cmd, BUILD_FOLDER"bpe", SRC_FOLDER"bpe.c")) return 1;
+    if (!build_tool(&cmd, BUILD_FOLDER"bpe2dot", SRC_FOLDER"bpe2dot.c")) return 1;
 
     return 0;
 }
