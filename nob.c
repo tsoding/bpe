fbf6b085076b6795792bad42cf16bc3434f3a1ce
rexim <reximkut@gmail.com>
2025-03-08 21:41:39 +0700

    [nob] build all the tools in parallel

--- nob.c
+++ nob.c
@@ -7,20 +7,23 @@
 #define SRC_FOLDER        "src/"
 #define THIRDPARTY_FOLDER "thirdparty/"
 
-bool build_tool(Cmd *cmd, const char *bin_path, const char *src_path)
+void build_tool(Cmd *cmd, Procs *procs, const char *bin_path, const char *src_path)
 {
     cmd_append(cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-I"THIRDPARTY_FOLDER, "-o", bin_path, src_path, SRC_FOLDER"bpe.c");
-    return cmd_run_sync_and_reset(cmd);
+    da_append(procs, cmd_run_async_and_reset(cmd));
 }
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
+    Procs procs = {0};
     if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
-    if (!build_tool(&cmd, BUILD_FOLDER"txt2bpe", SRC_FOLDER"txt2bpe.c")) return 1;
-    if (!build_tool(&cmd, BUILD_FOLDER"bpe2dot", SRC_FOLDER"bpe2dot.c")) return 1;
-    if (!build_tool(&cmd, BUILD_FOLDER"bpe_inspect", SRC_FOLDER"bpe_inspect.c")) return 1;
+
+    build_tool(&cmd, &procs, BUILD_FOLDER"txt2bpe", SRC_FOLDER"txt2bpe.c");
+    build_tool(&cmd, &procs, BUILD_FOLDER"bpe2dot", SRC_FOLDER"bpe2dot.c");
+    build_tool(&cmd, &procs, BUILD_FOLDER"bpe_inspect", SRC_FOLDER"bpe_inspect.c");
+    if (!procs_wait_and_reset(&procs)) return 1;
 
     return 0;
 }
